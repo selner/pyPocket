ad86241554054654fd0739884086d49919593adf
dev@bryanselner.com <dev@bryanselner.com>
2016-06-07 08:01:07 -0700

    * Began migration to using pscommon from selner/payscale as base module instead of duplicate functionality * Added initial support for exporting Confluence XML format for given tag * Improved image thumbnail display and wideneed layout for articles (replace constant value to match your own placeholder) * added missing python requirements file

--- getPocket.py
+++ getPocket.py
@@ -7,24 +7,27 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 import os, codecs
 import collections
-import operator
+from pscommon import psreport
 
-DAYSBACK=14
+ARTICLE_IMAGE_PLACEHOLDER = "https://confluence.underpaid.com/download/attachments/15436430/external_article_placeholder.jpg"
 
-class PocketMail():
+DAYSBACK=21
+
+class PocketMail(psreport.PSReportBase):
     config = None
     _all_articles = {}
     _prev_data_articles = {}
     _tag_processing_data = None
     _pocket_instance = None
     _tags_to_email = None
+    _email_html_template = None
+    _confluence_xml_template = None
 
     def __init__(self):
-        import bsconfig
-        self.config = bsconfig.BSConfig()
-        self.config.loadConfigFromFile("./config-bryan.ini")
+        psreport.PSReportBase.__init__(self, name="pyPocket")
         self._initPocket()
-        self._initTemplate()
+        self._email_html_template = self._initTemplate("email_html_template.mustache")
+        self._confluence_xml_template = self._initTemplate("confluence_template.mustache")
 
     def _initPocket(self):
         access_token = u'3d50b538-8bfc-d571-1488-fabf77'
@@ -56,6 +59,10 @@ class PocketMail():
                 dictTags[tag]['email'] = option[1]
             if len(option) > 2:
                 dictTags[tag]['last_processed'] = option[2]
+
+
+        # if len(dictTags) == 0:
+        #     dictTags = { 'tag' : "_ALL_ARTICLES_", 'email' : 'bryan@bryanselner.com' }
         return dictTags
 
                 #
@@ -66,13 +73,13 @@ class PocketMail():
                 # self.config.set(section="TagsToEmail", option=dictTagList[tag]['tag'], value=strCfgValue)
                 # self.saveConfigToFile()
 
-    def _initTemplate(self):
+    def _initTemplate(self, filepath):
 
         #
         #  Load the template
         #
-        self.config.logger.info("Reading template file 'email_html_template.mustache'...")
-        f = codecs.open("email_html_template.mustache", encoding='utf-8', mode='rb')
+        self.config.logger.info("Reading template file '" + filepath + "'...")
+        f = codecs.open(filepath, encoding='utf-8', mode='rb')
         strtempl = f.read()
         f.close()
 
@@ -81,11 +88,9 @@ class PocketMail():
         #
         from pybars import Compiler
         compiler = Compiler()
-        self.html_full_results_template = compiler.compile(strtempl)
+        return compiler.compile(strtempl)
 
-        self.email_html_template = compiler.compile(strtempl)
-
-    def getHTMLforArticleList(self, data):
+    def getXMLforArticleList(self, data, mustacheTemplate):
         self.config.logger.info("Generating html for article list...")
 
         if not (data and len(data)>0):
@@ -96,14 +101,30 @@ class PocketMail():
                 'articles' : data
             }
 
-        outHTML = self.email_html_template(data_for_template)
+        outHTML = mustacheTemplate(data_for_template)
 
         return outHTML
+
+    def export_xml_to_file(self, basename, template, articles=None):
+        xml = self.getXMLforArticleList(articles, template)
+
+        strtoday = datetime.date.today().strftime("%m-%d-%Y")
+        filename = strtoday+"_"+basename
+        filename = filename.replace(os.sep, "_")
+        fileout = os.path.join(self.config.output_folder, filename)
+
+        self.config.logger.info("Exporting test results to file '" + fileout + "'...")
+
+        f = codecs.open(fileout, encoding='utf-8', mode='w+')
+        f.write(xml)
+        f.close()
+
+        return fileout
 
     def export_html_to_file(self, basename, html=None, articles=None):
         if not html:
             if articles:
-                html = self.getHTMLforArticleList(articles)
+                html = self.getXMLforArticleList(articles, self._email_html_template)
 
         if not html:
             raise ValueError("Error: no html or articles to output to file")
@@ -120,6 +141,17 @@ class PocketMail():
         f.close()
 
         return fileout
+
+    def export_to_files(self, basename, articles=None):
+
+        self.config.logger.info("Exporting tag results to email and web html file...")
+        basefilename = "web_and_email_" + basename + ".html"
+        self.export_xml_to_file(basename=basefilename, template=self._email_html_template, articles=articles)
+
+        self.config.logger.info("Exporting tag results to confluence XML file...")
+        basefilename = "confluence_" + basename + ".xml"
+        self.export_xml_to_file(basename=basefilename, template=self._confluence_xml_template, articles=articles)
+
 
 
 
@@ -146,7 +178,8 @@ class PocketMail():
 
         results_content = collections.OrderedDict()
         for a in articles_by_date:
-
+            from datetime import datetime
+            datetime.fromtimestamp(int(articles[a]['time_added'])).strftime('%Y-%m-%d %H:%M:%S')
 
             if float(articles[a]['time_added']) >= sincetimestamp:
                 item = {}
@@ -162,21 +195,26 @@ class PocketMail():
                 item['authors'] = "-- " + authors_line
                 if 'tags' in articles[a]:
                     item['tags'] = [articles[a]['tags'][i]['tag'] for i in articles[a]['tags']]
-                item['added_date'] = datetime.fromtimestamp(float(articles[a]['time_added'])).strftime("%m/%d/%Y")
+#                item['datetime_added'] = datetime.fromtimestamp(int(articles[a]['time_added'])).strftime('%Y-%m-%d %H:%M:%S')
+                item['datetime_added'] = datetime.fromtimestamp(int(articles[a]['time_added'])).strftime('%Y-%m-%d')
                 item['time_added'] = float(articles[a]['time_added'])
                 if articles[a]['has_image'] == "1" and 'image' in articles[a]:
                     item['thumbnail'] = {}
                     item['thumbnail']['src'] = articles[a]['image']['src']
                     item['thumbnail']['href'] = item['href']
+                else:
+                    item['thumbnail'] = {}
+                    item['thumbnail']['src'] = ARTICLE_IMAGE_PLACEHOLDER
+                    item['thumbnail']['href'] = item['href']
 
-                results_content[item['added_date']] = item
+                results_content[item['time_added']] = item
 
         # sorted(results_content)
 
-        skeys = sorted(results_content.keys())
+        skeys = sorted(results_content.keys(), reverse=True)
         orderedArticles = collections.OrderedDict()
         for k in skeys:
-            orderedArticles[k] =results_content[k].copy()
+            orderedArticles[k] = results_content[k].copy()
 
         self.config.logger.log_pretty(level="INFO", data=orderedArticles, prefixtext="Articles returned: ")
         return orderedArticles
@@ -189,41 +227,44 @@ class PocketMail():
         :return: None
         """
 
-        if emailto:
-            try:
-                import uemail
-                emailob = uemail.UEmailSend()
-                emailob.setConnectionFromConfig(config=self.config)
-
-                self.config.logger.info("Sending report email to " + unicode(emailto))
-                emailob.sendEmail(sender=emailob._sender, recipient_list=emailto, subject=subject, html=html)
-                self.config.logger.info("Report sent.")
-            except (Exception, ValueError) as ex:
-                msg = "Unable to send report email: " + ex.message
-                self.config.logger.error(msg)
-                raise ValueError(msg)
+        self._sendReportViaEmail(emailto, subject, html)
 
 
 if __name__ == '__main__':
 
     pck = PocketMail()
 
-    dictTagList = pck.tags_to_email
+    # dictTagList = pck.tags_to_email
+    #
+    # items = pck.config.items("TagsToEmail")
+    #
+    # tagsToMatch = dictTagList.keys()
+    #
+    # if tagsToMatch:
+    #     for tag in tagsToMatch:
+    #
+    #         orderedArticles = pck.getArticlesSinceDate(numDays=180, tag=tag)
+    #         if len(orderedArticles) > 0:
+    #             html = pck.getHTMLforArticleList(data=orderedArticles)
+    #             htmlfile = pck.export_html_to_file(basename=tag + "_articles", html=html)
+    #             emailaddr = dictTagList[tag]['email']
+    #             pck.sendArticleListViaEmail(emailto=emailaddr, subject="New Saved Articles Tagged '" + tag + "'",
+    #                                         html=html)
+    #         else:
+    #             pck.config.logger.info("No articles found to export for tag '" + tag + "'.")
 
 
-    items = pck.config.items("TagsToEmail")
+    tagsToExport = ("powershell", "design", "dev")
 
-    tagsToMatch = dictTagList.keys()
+    for tag in tagsToExport:
+        orderedArticles = pck.getArticlesSinceDate(numDays=180, tag=tag )
+        if len(orderedArticles) > 0:
+            pck.export_to_files(basename=tag + "_articles", articles=orderedArticles)
 
-    if tagsToMatch:
-        for tag in tagsToMatch:
-
-            orderedArticles = pck.getArticlesSinceDate(numDays=30, tag=tag)
-            if len(orderedArticles) > 0:
-                html = pck.getHTMLforArticleList(data=orderedArticles)
-                htmlfile = pck.export_html_to_file(basename=tag+"_articles", html=html)
-                emailaddr = dictTagList[tag]['email']
-                pck.sendArticleListViaEmail(emailto=emailaddr, subject="New Saved Articles Tagged '" + tag + "'", html=html)
-            else:
-                pck.config.logger.info("No articles found to export for tag '" + tag + "'.")
+            # html = pck.getXMLforArticleList(data=orderedArticles)
+            # htmlfile = pck.export_html_to_file(basename=tag + "_articles", html=html)
+            # emailaddr = "bryan@bryanselner.com"
+            # pck.sendArticleListViaEmail(emailto=emailaddr, subject="New Saved Articles Tagged '" + tag + "'", html=html)
+        else:
+            pck.config.logger.info("No articles found to export for tag '" + tag + "'.")
 
